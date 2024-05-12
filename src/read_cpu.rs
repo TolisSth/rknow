aa28089671d49abddb92c7f888fd765b07113bbd
TolisSth <tolishalis@gmail.com>
2024-05-12 19:16:49 +0300

    Getting first line

--- src/read_cpu.rs
+++ src/read_cpu.rs
@@ -3,15 +3,16 @@ use std::fs;
 
 pub fn read_proc_stat(){
     let proc_stat_content = fs::read_to_string("/proc/stat").expect("Error: Can not read /proc/stat"); 
-    let proc_stat_content_vec: Vec<&str> = proc_stat_content.lines().collect(); // Get first line
-    
-    let mut cpu_data: Vec<u32> = Vec::new();
+    let mut proc_stat_first_line: String = Default::default(); 
 
-    for ch in proc_stat_content_vec[0].enumerate(){
-        if ch.is_numeric(){
-            cpu_data.push(ch); 
+    for ch in proc_stat_content.chars(){ 
+        if ch != '\n'{
+            proc_stat_first_line.push(ch);  
+        }
+        else{
+            break; 
         }
     }
-
-    println!("{:?}", cpu_data); 
+    
+    println!("{}", proc_stat_first_line); 
 }
