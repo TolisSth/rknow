4934fbb07491d5f9e54ae567d9c4ae8017269e58
Apostolos Chalis <tolishalis@gmail.com>
2024-06-15 15:48:11 +0300

    Added daemonize.rs to handle daemonization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 // Author: Apostolos Chalis 2024
 mod read_cpu; 
+mod daemonize; 
 
 fn main() {
     println!("rknow ~ A Rust system profiler v0.1.0\nApostolos Chalis 2024");
+
+    println!("Starting daemon..."); 
+    daemonize::daemonize();
+
     read_cpu::read_proc_stat(); 
 }
